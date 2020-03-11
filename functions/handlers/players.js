99bfec1a0921964ecbcdf0c1456db58b3cd3cff0
Nando Linhares <fernandolinhares1920@globo.com>
2020-03-11 20:30:09 -0300

    createPlayer admin validation done

--- functions/handlers/players.js
+++ functions/handlers/players.js
@@ -4,25 +4,53 @@ const config = require('../util/config');
 const BusBoy = require('busboy');
 
 exports.createPlayer = (req, res) => {
-    const noImg = 'no-img.png';  
-    const playerData = {
-        name: req.body.name,
-        position: req.body.position,
-        team: req.body.team,
-        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
-        price: req.body.price,
-        createdAt: new Date().toISOString()
-    }
 
-    db.collection('players').add(playerData)
-        .then(doc => {
-            playerData.id = doc.id;
-            res.json(playerData);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ error: err.code });
-        })
+    const isAdmin = req.user.administrator;
+
+    if(isAdmin) {
+        const noImg = 'no-img.png';  
+        const playerData = {
+            name: req.body.name,
+            position: req.body.position,
+            team: req.body.team,
+            imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
+            price: req.body.price,
+            createdAt: new Date().toISOString()
+        }
+
+        db.collection('players').where('name', '==', playerData.name).get()
+            .then(data => {
+                const error = {};
+                data.forEach(doc => {
+                    if(doc.exists){
+                       error.status =  true;
+                       error.message = 'O jogador já existe no sistema';    
+                    }
+                });
+                return error;
+            })
+            .then((error) => {
+                if(error.status === true){
+                    return res.status(400).json(error);
+                } else {
+                    db.collection('players').add(playerData)
+                        .then(doc => {
+                            playerData.id = doc.id;
+                            res.json(playerData);
+                        })
+                        .catch(err => {
+                            console.error(err);
+                            res.status(500).json({ error: err.code });
+                        })
+                }
+            })
+            .catch(err => console.error(err));
+
+            
+              
+    } else {
+        return res.status(401).json({ message: 'Você não tem autorização para cadastrar um jogador.' });
+    }
 }
 
 //Upload image
@@ -61,11 +89,15 @@ exports.uploadPlayerImage = (req, res) => {
         })
         .then(() => {
             const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
-            return db.collection(`/players`).where('name', '==', req.params.name).get()
+            return db.collection(`players`).where('name', '==', req.params.name).get()
                 .then(data => {
                    data.forEach(doc => {
                        doc.ref.update({ imageUrl })
                    })
+                })
+                .catch(err => {
+                    console.error(err);
+                    res.status(500).json({ error: err.code });
                 })
         })
         .then(() => {
