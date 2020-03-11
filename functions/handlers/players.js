d62ee1d45e4b647c6469585d6f015604ca5a2397
Nando Linhares <fernandolinhares1920@globo.com>
2020-03-11 20:55:35 -0300

    validating updatePlayerImage

--- functions/handlers/players.js
+++ functions/handlers/players.js
@@ -59,55 +59,61 @@ exports.uploadPlayerImage = (req, res) => {
     const os = require('os'); //igual
     const fs = require('fs'); //file system
 
+    const isAdmin = req.user.administrator;
+
     const busboy = new BusBoy({ headers: req.headers }); 
 
     let imageFileName;
     let imageToBeUploaded = {}; //Empty Object
 
-    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-       if(mimetype !== 'image/jpeg' && mimetype !== 'image/png'){
-           return res.status(400).json({ error: 'O arquivo não é válido' });
-       }
-        //my.image.png
-        const imageExtension = filename.split('.')[filename.split('.').length -1];
-        //46498484984.png
-        imageFileName = `${Math.round(Math.random() * 100000000000)}.${imageExtension}`;
-        const filepath = path.join(os.tmpdir(), imageFileName);
-        imageToBeUploaded = { filepath, mimetype }
-
-        file.pipe(fs.createWriteStream(filepath));
-    });
-
-    busboy.on('finish', () => {
-        admin.storage().bucket(config.storageBucket).upload(imageToBeUploaded.filepath, {
-            resumable: false,
-            metadata: {
-                metadata: {
-                    contentType: imageToBeUploaded.mimetype
-                }
+    if(isAdmin){
+        busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+            if(mimetype !== 'image/jpeg' && mimetype !== 'image/png'){
+                return res.status(400).json({ error: 'O arquivo não é válido' });
             }
-        })
-        .then(() => {
-            const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
-            return db.collection(`players`).where('name', '==', req.params.name).get()
-                .then(data => {
-                   data.forEach(doc => {
-                       doc.ref.update({ imageUrl })
-                   })
-                })
-                .catch(err => {
-                    console.error(err);
-                    res.status(500).json({ error: err.code });
-                })
-        })
-        .then(() => {
-            return res.json({ message: 'Imagem atualizada com sucesso' });
-        })
-        .catch(err => {
-            console.error(err);
-            return res.status(500).json({ error: err.code });
-        });
-    });
+             //my.image.png
+             const imageExtension = filename.split('.')[filename.split('.').length -1];
+             //46498484984.png
+             imageFileName = `${Math.round(Math.random() * 100000000000)}.${imageExtension}`;
+             const filepath = path.join(os.tmpdir(), imageFileName);
+             imageToBeUploaded = { filepath, mimetype }
+     
+             file.pipe(fs.createWriteStream(filepath));
+         });
+     
+         busboy.on('finish', () => {
+            admin.storage().bucket(config.storageBucket).upload(imageToBeUploaded.filepath, {
+                resumable: false,
+                metadata: {
+                    metadata: {
+                        contentType: imageToBeUploaded.mimetype
+                    }
+                }
+            })
+            .then(() => {
+                const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+                return db.collection(`players`).where('name', '==', req.params.name).get()
+                    .then(data => {
+                    data.forEach(doc => {
+                        doc.ref.update({ imageUrl })
+                    })
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        res.status(500).json({ error: err.code });
+                    })
+            })
+            .then(() => {
+                return res.json({ message: 'Imagem atualizada com sucesso' });
+            })
+            .catch(err => {
+                console.error(err);
+                return res.status(500).json({ error: err.code });
+            });
+         });
+    } else {
+        return res.status(401).json({ error: 'Você não tem autorização para isso' });
+    }
 
     busboy.end(req.rawBody);
 };
