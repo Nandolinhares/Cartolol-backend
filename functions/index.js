a7cf174d0b760ac2d795b0f1b62b3c34cce2b137
Nando Linhares <fernandolinhares1920@globo.com>
2020-03-17 20:02:57 -0300

    updated index. Created trigger onUpdate to change all the player informations

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,9 @@ const functions = require('firebase-functions');
 //Cors
 const cors = require('cors');
 
+const { db } = require('./util/admin');
+const FieldValue = require('firebase-admin').firestore.FieldValue;
+
 //Express stuffs
 const express = require('express');
 const app = express();
@@ -40,4 +43,26 @@ app.get('/posts', FBAuth, getAllPosts);
 
 exports.api = functions.region('us-east1').https.onRequest(app);
 
+//Usando a definição de wildcard do firebase
+//In this example, when any field on any document in users is changed, it matches a wildcard called playerId.
+ exports.onPlayerDetailsChange = functions.region('us-east1').firestore.document('/players/{playerId}')
+    .onUpdate((change) => {
+
+        let batch = db.batch();
+
+        if(change.before.data() !== change.after.data()){
+            db.collection('users').where("userTeam", "array-contains" , change.before.data()).get()
+                .then(data => {
+                    data.forEach(doc => {
+                        const user = db.doc(`/users/${doc.id}`);
+                        batch.update(user, { userTeam: FieldValue.arrayRemove(change.before.data()) });
+                        batch.update(user, { userTeam: FieldValue.arrayUnion(change.after.data()) });
+                    })
+                    return batch.commit();
+                })
+                .catch(err => console.error(err));
+        } else {
+            return true;
+        }
+    })
 
